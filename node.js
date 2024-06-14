564622abdfc30fc14fbafcb238552307d45613c5
Carl Wico <wicocarl12@gmail.com>
2024-06-14 16:45:48 -0230

    Created mostly the functions

--- node.js
+++ node.js
@@ -1,5 +1,7 @@
-let computerChoice = "";
+let computerScore = 0;
+let playerScore = 0;
 
+// Function to get the computer choice
 function getComputerChoice() {
   randomNumber = Math.random();
 
@@ -11,4 +13,45 @@ function getComputerChoice() {
 
   return computerChoice;
 }
-computerChoice = getComputerChoice();
+
+// Function to display the score for player and computer
+function displayScore() {
+  console.log(`Player Score: ${playerScore} : Computer Score ${computerScore}`);
+}
+
+// Function to display the choices for player and computer
+function displayChoices() {
+  console.log(
+    `Player picked ${playerChoice} : Computer picked ${computerChoice}`
+  );
+}
+
+// Function to get that calculates the outcome who won
+function getOutcome(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    displayChoices();
+    console.log("It's a tie!");
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    displayChoices();
+    playerScore += 1;
+    console.log("Player Wins!");
+    displayScore();
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    displayChoices();
+    playerScore += 1;
+    console.log("Player Wins!");
+    displayScore();
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    displayChoices();
+    playerScore += 1;
+    console.log("Player Wins!");
+    displayScore();
+  }
+}
+// Function to play the round
+function playRound() {
+  let playerChoice = prompt("Rock, Paper, or Scissors: ");
+  let computerChoice = getComputerChoice();
+
+  getOutcome(playerChoice, computerChoice);
+}
